26385943aef2601b615fd3f3700ed955c2b75f6b
Ingo Schwarz <ingo.schwarz23@gmail.com>
2023-10-12 21:00:41 +0200

    Implement LESS SLOW solution for level 5 - challenge 1

--- src/main/java/com/google/foobar/level5/challenge1/Solution.java
+++ src/main/java/com/google/foobar/level5/challenge1/Solution.java
@@ -1,35 +1,28 @@
 package com.google.foobar.level5.challenge1;
 
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Spliterators;
+import java.util.Set;
 import java.util.function.Function;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 public class Solution {
 
-    public static String solution(int girdWidth, int gridHeight, int numberOfStates) {
-
-        final List<Crystal> knownValues = new ArrayList<>();
-        final long count = createCrystalStream(girdWidth, gridHeight, numberOfStates)
-                .filter(c -> {
-                    boolean known = knownValues.contains(c);
-                    if (!known) {
-                        knownValues.add(c);
-                    }
-                    return !known;
-                })
-                // .peek(System.out::println)
-                .count();
-        return String.valueOf(count);
+    public static String solution(int gridWidth, int gridHeight, int numberOfStates) {
+        Set<Crystal> crystals = new HashSet<>();
+        createCrystalIterable(gridWidth, gridHeight, numberOfStates).forEach(c -> {
+            if (!crystals.contains(c)) {
+                crystals.removeAll(getVerticalOrHorizontalPermutations(c));
+                crystals.add(c);
+            }
+        });
+        return Long.toUnsignedString(crystals.size());
     }
 
-    private static Stream<Crystal> createCrystalStream(final int girdWidth, final int gridHeight, final int numberOfStates) {
-        Iterator<Crystal> iterator = new Iterator<Crystal>() {
+    private static Iterable<Crystal> createCrystalIterable(final int girdWidth, final int gridHeight, final int numberOfStates) {
+        return () -> new Iterator<Crystal>() {
 
             private final int[][] currentState = new int[gridHeight][girdWidth];
             private final long maxStateCount = (long) Math.pow(numberOfStates, gridHeight * girdWidth);
@@ -58,7 +51,7 @@ public class Solution {
                 }
                 // create new state
                 stateCount++;
-                return new Crystal(cloneArray(currentState));
+                return new Crystal(deepCopy(currentState));
             }
 
             private int getValueAt(int index) {
@@ -74,7 +67,71 @@ public class Solution {
             }
 
         };
-        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(iterator, 0), false);
+    }
+
+    private static Set<Crystal> getVerticalOrHorizontalPermutations(Crystal crystal) {
+        return getVerticalOrHorizontalPermutations(crystal.states.length, crystal.states);
+    }
+
+    private static Set<Crystal> getVerticalOrHorizontalPermutations(int n, int[][] elements) {
+
+        if (n == 1) {
+            Set<Crystal> permutations = new HashSet<>();
+            permutations.add(new Crystal(deepCopy(elements)));
+            permutations.addAll(getHorizontalPermutations(elements[0].length, deepCopy(elements)));
+            return permutations;
+        } else {
+            Set<Crystal> permutations = new HashSet<>(getVerticalOrHorizontalPermutations(n - 1, elements));
+            for (int i = 0; i < n - 1; i++) {
+                if (n % 2 == 0) {
+                    swapRows(elements, i, n - 1);
+                } else {
+                    swapRows(elements, 0, n - 1);
+                }
+                permutations.addAll(getVerticalOrHorizontalPermutations(n - 1, elements));
+            }
+            return permutations;
+        }
+    }
+
+    private static Set<Crystal> getHorizontalPermutations(int n, int[][] elements) {
+
+        if (n == 1) {
+            return Collections.singleton(new Crystal(deepCopy(elements)));
+        } else {
+            Set<Crystal> permutations = new HashSet<>(getHorizontalPermutations(n - 1, elements));
+            for (int i = 0; i < n - 1; i++) {
+                if (n % 2 == 0) {
+                    swapColumns(elements, i, n - 1);
+                } else {
+                    swapColumns(elements, 0, n - 1);
+                }
+                permutations.addAll(getHorizontalPermutations(n - 1, elements));
+            }
+            return permutations;
+        }
+    }
+
+    private static void swapRows(int[][] elements, int a, int b) {
+        int[] tmp = elements[a];
+        elements[a] = elements[b];
+        elements[b] = tmp;
+    }
+
+    private static void swapColumns(int[][] elements, int a, int b) {
+        for (int row = 0; row < elements.length; row++) {
+            int tmp = elements[row][a];
+            elements[row][a] = elements[row][b];
+            elements[row][b] = tmp;
+        }
+    }
+
+    private static int[][] deepCopy(int[][] arr) {
+        int[][] clone = new int[arr.length][];
+        for (int i = 0; i < arr.length; i++) {
+            clone[i] = arr[i].clone();
+        }
+        return clone;
     }
 
     public static class Crystal {
@@ -100,110 +157,14 @@ public class Solution {
         public boolean equals(Object o) {
             if (this == o) return true;
             if (!(o instanceof Crystal)) return false;
-            Crystal crystal = (Crystal) o;
-            return matchesAnyVerticalOrHorizontalPermutation(states.length, cloneArray(states), crystal.states);
+            Crystal other = (Crystal) o;
+            return Arrays.deepEquals(this.states, other.states);
         }
 
         @Override
         public int hashCode() {
-            return generateHashUsingVerticalAndHorizontalPermutation(states.length, cloneArray(states));
+            return Arrays.deepHashCode(states);
         }
-    }
-
-    private static boolean matchesAnyVerticalOrHorizontalPermutation(int n, int[][] elements, int[][] goal) {
-
-        if (n == 1) {
-            return Arrays.deepEquals(elements, goal) || matchesUsingHorizontalPermutation(goal[0].length, cloneArray(elements), goal);
-        } else {
-            for (int i = 0; i < n - 1; i++) {
-                if (matchesAnyVerticalOrHorizontalPermutation(n - 1, elements, goal)) {
-                    return true;
-                }
-                if (n % 2 == 0) {
-                    swapRows(elements, i, n - 1);
-                } else {
-                    swapRows(elements, 0, n - 1);
-                }
-            }
-            return matchesAnyVerticalOrHorizontalPermutation(n - 1, elements, goal);
-        }
-    }
-
-    private static boolean matchesUsingHorizontalPermutation(int n, int[][] elements, int[][] goal) {
-
-        if (n == 1) {
-            return Arrays.deepEquals(elements, goal);
-        } else {
-            for (int i = 0; i < n - 1; i++) {
-                if (matchesUsingHorizontalPermutation(n - 1, elements, goal)) {
-                    return true;
-                }
-                if (n % 2 == 0) {
-                    swapColumns(elements, i, n - 1);
-                } else {
-                    swapColumns(elements, 0, n - 1);
-                }
-            }
-            return matchesUsingHorizontalPermutation(n - 1, elements, goal);
-        }
-    }
-
-    private static int generateHashUsingVerticalAndHorizontalPermutation(int n, int[][] elements) {
-
-        if (n == 1) {
-            return 31 * Arrays.deepHashCode(elements) + generateHashUsingHorizontalPermutation(elements[0].length, cloneArray(elements));
-        } else {
-            int hash = generateHashUsingVerticalAndHorizontalPermutation(n - 1, elements);
-            for (int i = 0; i < n - 1; i++) {
-                if (n % 2 == 0) {
-                    swapRows(elements, i, n - 1);
-                } else {
-                    swapRows(elements, 0, n - 1);
-                }
-                hash = 31 * hash + generateHashUsingVerticalAndHorizontalPermutation(n - 1, elements);
-            }
-            return hash;
-        }
-    }
-
-    private static int generateHashUsingHorizontalPermutation(int n, int[][] elements) {
-
-        if (n == 1) {
-            return Arrays.deepHashCode(elements);
-        } else {
-            int hash = generateHashUsingHorizontalPermutation(n - 1, elements);
-            for (int i = 0; i < n - 1; i++) {
-                if (n % 2 == 0) {
-                    swapColumns(elements, i, n - 1);
-                } else {
-                    swapColumns(elements, 0, n - 1);
-                }
-                hash = 31 * hash + generateHashUsingHorizontalPermutation(n - 1, elements);
-            }
-            return hash;
-        }
-    }
-
-    private static void swapRows(int[][] elements, int a, int b) {
-        int[] tmp = elements[a];
-        elements[a] = elements[b];
-        elements[b] = tmp;
-    }
-
-    private static void swapColumns(int[][] elements, int a, int b) {
-        for (int row = 0; row < elements.length; row++) {
-            int tmp = elements[row][a];
-            elements[row][a] = elements[row][b];
-            elements[row][b] = tmp;
-        }
-    }
-
-    private static int[][] cloneArray(int[][] arr) {
-        int[][] clone = new int[arr.length][];
-        for (int i = 0; i < arr.length; i++) {
-            clone[i] = arr[i].clone();
-        }
-        return clone;
     }
 
 }
